6cae19a64002084817664aa8b9eb50f724de6001
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-20 20:22:37 +0000

    Initial Commit

---
+++ CostumRegistration/CostumRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostumRegistrationApp.swift
+//  CostumRegistration
+//
+//  Created by Abdoulaye Aliou SALL on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CostumRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
